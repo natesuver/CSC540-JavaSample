eb8f815f847ba0f6318ee84f850383fc2cca8f21
Nathan Suver <nate.suver@gmail.com>
2023-10-07 10:27:49 -0400

    sample

--- src/Ui/Startup.java
+++ src/Ui/Startup.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 import DataAccess.Dropdowns;
@@ -16,6 +17,7 @@ import javax.swing.JTable;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.JList;
 import javax.swing.AbstractListModel;
 import javax.swing.JLabel;
@@ -32,7 +34,6 @@ public class Startup extends JFrame {
 	private int offset = 0;
 	private final int maxValuesToList = 3;
 	private JLabel lblTotalResults;
-	
 	
 	/**
 	 * Launch the application.
@@ -54,19 +55,50 @@ public class Startup extends JFrame {
 	 * Create the frame.
 	 */
 	public Startup() {
+		var showDropdownExample = false;
+		var showPaginationExample = true;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		
+
 		JComboBox comboBox = new JComboBox();
 		var result = Dropdowns.GetStates();
 		for (var state : result) {
 			comboBox.addItem(state);
 		}
 		
+
+		
+		stateField = new JTextField();
+		
+		stateField.setColumns(10);
+		
+		JButton btnFilterState = new JButton("Filter State");
+
+
+		if (showDropdownExample) {
+			contentPane.add(stateField);
+			contentPane.add(btnFilterState);
+			contentPane.add(comboBox);
+		}
+
+		
+		btnFilterState.addActionListener(new ActionListener() {
+		
+		
+		
+		
+		@Override
+		public void actionPerformed(java.awt.event.ActionEvent e) {
+			AddState(comboBox);
+		}
+
+			
+		});
+
 		btnNext = new JButton("Next");
 		btnNext.addActionListener(new ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent e) {
@@ -84,42 +116,24 @@ public class Startup extends JFrame {
 		});
 		
 		lblTotalResults = new JLabel("New label");
-		contentPane.add(lblTotalResults);
-		contentPane.add(btnBack);
-		contentPane.add(btnNext);
+
+	
 		int totalCount = Dropdowns.GetStateCount();
 		lblTotalResults.setText("We found " + totalCount + " results today");
 		list_1 = new JList();
 		SetListModel(list_1);
-		contentPane.add(list_1);
 		
-		stateField = new JTextField();
-		contentPane.add(stateField);
-		stateField.setColumns(10);
-		
-		JButton btnFilterState = new JButton("Filter State");
-		contentPane.add(btnFilterState);
-		
-		contentPane.add(comboBox);
-		//Owner ownerInstance = new Owner();
-		//ownerInstance.setState(stateField.getText());
-		//ownerInstance.setZip("06574");
-		//ownerInstance.Add("");
-		
-		
-		btnFilterState.addActionListener(new ActionListener() {
-		
-		
-		
-		
-		@Override
-		public void actionPerformed(java.awt.event.ActionEvent e) {
-			AddState(comboBox);
+		if (showPaginationExample) {
+			contentPane.add(lblTotalResults);
+			contentPane.add(btnBack);
+			contentPane.add(btnNext);
+			contentPane.add(list_1);
 		}
 
-			
-		});
+
+
 	}
+
 
 	private void AddState(JComboBox comboBox) {
 		//System.out.println("CLICKED");
